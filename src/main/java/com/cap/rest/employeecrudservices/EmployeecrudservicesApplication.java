ebf60598b226b258175043bab9d9f277d97ba835
Nimmala <mrudula.nimmala@capgemini.com>
2019-01-16 20:30:06 +0530

    adds  mongodb

--- src/main/java/com/cap/rest/employeecrudservices/EmployeecrudservicesApplication.java
+++ src/main/java/com/cap/rest/employeecrudservices/EmployeecrudservicesApplication.java
@@ -1,31 +1,17 @@
 package com.cap.rest.employeecrudservices;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.cap.rest.employeecrudservices.entity.Address;
-import com.cap.rest.employeecrudservices.entity.Employee;
-import com.cap.rest.employeecrudservices.repo.EmployeeRepository;
 
 @SpringBootApplication
-public class EmployeecrudservicesApplication {
+public class EmployeecrudservicesApplication  {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeecrudservicesApplication.class, args);
 	}
+
 	
-	@Bean
-	public CommandLineRunner populateData(EmployeeRepository repository) {
-		return (arg)-> {
-			repository.save(new Employee(101,"Bhargavi",new Address(12,"Sai Ram Colony","Anakapalli","Andhra Pradesh",432516)));
-			repository.save(new Employee(102,"Pavani",new Address(14,"Housing Board Colony","Mangalagiri","Andhra Pradesh",65433)));
-			repository.save(new Employee(103,"Mrudula",new Address(19,"Rkc New Colony","Macherla","Andhra Pradesh",522426)));
-			repository.save(new Employee(104,"Vineela",new Address(16,"Hindu College of Pharmacy","Guntur","Andhra Pradesh",522408)));
-		};
-	}
 	
 
 }
-
--- src/main/java/com/cap/rest/employeecrudservices/repo/EmployeeRepository.java
+++ src/main/java/com/cap/rest/employeecrudservices/repo/EmployeeRepository.java
@@ -1,11 +1,11 @@
 package com.cap.rest.employeecrudservices.repo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cap.rest.employeecrudservices.entity.Employee;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+public interface EmployeeRepository extends MongoRepository<Employee, Object>{
 
 }
